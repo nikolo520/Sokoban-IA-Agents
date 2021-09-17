a2b8de8b9996891039d5090b114c6ae64cde8327
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-17 01:43:21 -0500

    add Update

--- src/sokoban/SokobanProblem.js
+++ src/sokoban/SokobanProblem.js
@@ -7,6 +7,188 @@ class SokobanProblem extends Problem {
     }
 
     /**
+     * The transition model. 
+     * Tells how to change the state (data) based on the given actions. You must override
+     * In this case, the actions can be one the four movements or the TAKE action.
+     * In this case, what changes based on the movement actions is the x or y position of the agent
+     * or the current cell if the action is TAKE
+     * @param {} data 
+     * @param {*} action 
+     * @param {*} agentID 
+     */
+     update(data, action, agentID) {
+        let map = data.world;
+        let agentState = data.states[agentID];
+        if (action == "UP") {
+            next_position = {'x':agentState.x,'y':agentState.y - 1}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    if(this.shove_box(data,action,next_position)){
+                        agentState.y -= 1;
+                    }
+                }else{
+                    agentState.y -= 1;
+                }
+            }
+            
+        }
+        if (action == "DOWN") {
+            next_position = {'x':agentState.x,'y':agentState.y + 1}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    if(this.shove_box(data,action,next_position)){
+                        agentState.y += 1;
+                    }
+                }else{
+                    agentState.y += 1;
+                }
+            }
+        }
+        if (action == "LEFT") {
+            agentState.x -= 1;
+            next_position = {'x':agentState.x -1,'y':agentState.y}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    if(this.shove_box(data,action,next_position)){
+                        agentState.x -= 1;
+                    }
+                }else{
+                    agentState.x -= 1;
+                }
+            }
+        }
+        if (action == "RIGHT") {
+            agentState.x += 1;
+            next_position = {'x':agentState.x +1,'y':agentState.y}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    if(this.shove_box(data,action,next_position)){
+                        agentState.x += 1;
+                    }
+                }else{
+                    agentState.x += 1;
+                }
+            }
+        }
+        if (!data.interations) {
+            data.interations = 1;
+        } else {
+            data.interations++;
+        }
+    }
+
+    shove_box(data,action,position){
+        let map = data.world;
+               
+        if (action == "UP") {
+            next_position = {'x':position.x,'y':position.y - 1}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    return false;
+                }else{
+                    if(this.is_goal(data.world,next_position)){
+                        data.world[next_position.y][next_position.x] = 'Z';
+                    }
+                    for(var i = 0; i < data.box_points.length(); i++) {
+                        if(box_points[0] == [position.x,position.y]){
+                            box_points[0][1] = next_position.y
+                        }
+                    }
+                    return true;
+                }
+            }else{
+                return false;
+            }
+        }
+        if (action == "DOWN") {
+            next_position = {'x':position.x,'y':position.y + 1}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    return false;
+                }else{
+                    if(this.is_goal(data.world,next_position)){
+                        data.world[next_position.y][next_position.x] = 'Z';
+                    }
+                    for(var i = 0; i < data.box_points.length(); i++) {
+                        if(box_points[0] == [position.x,position.y]){
+                            box_points[0][1] = next_position.y
+                        }
+                    }
+                    return true;
+                }
+            }else{
+                return false;
+            }
+        }
+        if (action == "LEFT") {
+            next_position = {'x':position.x -1,'y':position.y}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    return false;
+                }else{
+                    if(this.is_goal(data.world,next_position)){
+                        data.world[next_position.y][next_position.x] = 'Z';
+                    }
+                    for(var i = 0; i < data.box_points.length(); i++) {
+                        if(box_points[0] == [position.x,position.y]){
+                            box_points[0][0] = next_position.x
+                        }
+                    }
+                    return true;
+                }
+            }else{
+                return false;
+            }
+        }
+        if (action == "RIGHT") {
+            next_position = {'x':position.x +1,'y':position.y}
+            if(this.is_empty(map , next_position)){
+                if(this.is_box(data.box_points, next_position)){
+                    return false;
+                }else{
+                    if(this.is_goal(data.world,next_position)){
+                        data.world[next_position.y][next_position.x] = 'Z';
+                    }
+                    for(var i = 0; i < data.box_points.length(); i++) {
+                        if(box_points[0] == [position.x,position.y]){
+                            box_points[0][0] = next_position.x
+                        }
+                    }
+                    return true;
+                }
+            }else{
+                return false;
+            }
+        }
+    }
+
+    is_goal(world,point){
+        if(world[point.y][point.x] == 'X'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    is_empty(world,point){
+        if(world[point.y][point.x] == '0' || world[point.y][point.x] == 'X'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    is_box(box_points,point){
+        result = false;
+        for(var i = 0; i < box_points.length(); i++) {
+            if(box_points[0] == [point.x,point.y]){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    /**
      * Gives the world representation for the agent at the current stage.
      * Notice that the agent don't have access to the whole labyrinth. It only "see"
      * the cell around and under it. 
