a98131f16a660ab1e3bd7ee1487dd986ac9a61b5
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-17 01:42:32 -0500

    add Perception

--- src/sokoban/SokobanProblem.js
+++ src/sokoban/SokobanProblem.js
@@ -7,6 +7,35 @@ class SokobanProblem extends Problem {
     }
 
     /**
+     * Gives the world representation for the agent at the current stage.
+     * Notice that the agent don't have access to the whole labyrinth. It only "see"
+     * the cell around and under it. 
+     * @param {*} agentID 
+     * @returns and object with the information to be sent to the agent
+     */
+     perceptionForAgent(data, agentID) {
+        let map = data.world;
+        let agentState = data.states[agentID];
+        let x = agentState.x;
+        let y = agentState.y;
+        let result = [];
+        //LEFT
+        result.push(x > 0 ? map[y][x - 1] : 1);
+        //UP
+        result.push(y > 0 ? map[y - 1][x] : 1);
+        //RIGTH
+        result.push(x < map[0].length - 1 ? map[y][x + 1] : 1);
+        //DOWN
+        result.push(y < map.length - 1 ? map[y + 1][x] : 1);
+
+        result = result.map(value => value > 0 ? 1 : 0);
+
+        //SMELL
+        result.push(Math.abs(map[y][x]));
+        return result;
+    }
+
+    /**
      * Check if the given solution solves the problem. You must override.
      * The current state of the enviroment is maintained in data.world
      * @param {Object} solution 
