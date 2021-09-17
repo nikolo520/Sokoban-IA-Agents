25f7dae88b2d6be5c4d70d16238f6cd573b794b7
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-17 01:39:05 -0500

    Add Sokoban Problem

---
+++ src/sokoban/SokobanProblem.js
@@ -0,0 +1,12 @@
+const Problem = require('../core/Problem');
+
+class SokobanProblem extends Problem {
+    constructor(args) {
+        super(args);
+        this.env = args;
+    }
+
+    
+}
+
+module.exports = SokobanProblem;
