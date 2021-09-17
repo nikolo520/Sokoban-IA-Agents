07fcb935f573af6a6a4206905a811c39b203436c
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-17 01:40:26 -0500

    add solve

--- src/sokoban/SokobanProblem.js
+++ src/sokoban/SokobanProblem.js
@@ -6,7 +6,19 @@ class SokobanProblem extends Problem {
         this.env = args;
     }
 
-    
+
+
+    /**
+     * Solve the given problem
+     * @param {*} world 
+     * @param {*} box_points 
+     * @param {*} callbacks  
+     */
+
+    solve(world, box_points, callbacks) {
+        this.controller.setup({ world: world, box_points:box_points,problem: this });
+        this.controller.start(callbacks, false);
+    }
 }
 
 module.exports = SokobanProblem;
